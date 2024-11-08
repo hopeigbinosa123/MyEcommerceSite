cf8c99a9e3752a7fa9c83422008a0910325c51f0
hope Igbinoba <111630507+hopeigbinosa123@users.noreply.github.com>
2024-11-08 10:41:03 +0200

    fixed the error of template

--- Backend/blog/views.py
+++ Backend/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html')
+
+def home(request):
+    return render(request, 'blog/index.html')
